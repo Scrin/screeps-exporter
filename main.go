9764a1d4aabf34de256cc90c3df70811ce7f0620
Scrin <Microx256@gmail.com>
2021-07-06 22:35:54 +0300

    Add some account data and market orders

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"compress/gzip"
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -16,15 +13,30 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-type MemoryResponse struct {
-	Ok   int64  `json:"ok"`
-	Data string `json:"data"`
+type AuthMeResponse struct {
+	Money     float64            `json:"money"`
+	CPUShard  map[string]float64 `json:"cpuShard"`
+	Resources map[string]float64 `json:"resources"`
 }
 
-type Memory struct {
-	Stats struct {
-		History []Stats `json:"history"`
-	} `json:"stats"`
+type MarketOrder struct {
+	Active          bool    `json:"active"`
+	Type            string  `json:"type"`
+	Amount          float64 `json:"amount"`
+	RemainingAmount float64 `json:"remainingAmount"`
+	ResourceType    string  `json:"resourceType"`
+	Price           float64 `json:"price"`
+	TotalAmount     float64 `json:"totalAmount"`
+	RoomName        string  `json:"roomName"`
+}
+
+type MarketOrdersResponse struct {
+	Shards map[string][]MarketOrder `json:"shards"`
+}
+
+type MemoryResponse struct {
+	Ok   float64 `json:"ok"`
+	Data string  `json:"data"`
 }
 
 type Cpu struct {
@@ -34,24 +46,24 @@ type Cpu struct {
 }
 
 type Progress struct {
-	Level         int64   `json:"level"`
+	Level         float64 `json:"level"`
 	Progress      float64 `json:"progress"`
 	ProgressTotal float64 `json:"progressTotal"`
 }
 
 type Room struct {
-	RCL                     Progress         `json:"rcl"`
-	Structures              map[string]int64 `json:"structures"`
-	Creeps                  int64            `json:"creeps"`
-	EnergyAvailable         float64          `json:"energyAvailable"`
-	EnergyCapacityAvailable float64          `json:"energyCapacityAvailable"`
-	Storage                 map[string]int64 `json:"storage"`
-	Terminal                map[string]int64 `json:"terminal"`
+	RCL                     Progress           `json:"rcl"`
+	Structures              map[string]float64 `json:"structures"`
+	Creeps                  float64            `json:"creeps"`
+	EnergyAvailable         float64            `json:"energyAvailable"`
+	EnergyCapacityAvailable float64            `json:"energyCapacityAvailable"`
+	Storage                 map[string]float64 `json:"storage"`
+	Terminal                map[string]float64 `json:"terminal"`
 }
 
 type Stats struct {
-	Tick  uint64          `json:"tick"`
-	Ms    uint64          `json:"ms"`
+	Tick  float64         `json:"tick"`
+	Ms    float64         `json:"ms"`
 	CPU   Cpu             `json:"cpu"`
 	GCL   Progress        `json:"gcl"`
 	GPL   Progress        `json:"gpl"`
@@ -69,6 +81,9 @@ var (
 
 	knownRooms map[string]prometheus.Labels
 
+	cpuShard           *prometheus.GaugeVec
+	resources          *prometheus.GaugeVec
+	marketOrders       *prometheus.GaugeVec
 	tick               *prometheus.GaugeVec
 	ms                 *prometheus.GaugeVec
 	cpu                *prometheus.GaugeVec
@@ -88,7 +103,20 @@ func setup() {
 	typedLabels := append(baseLabels, "type")
 	roomLabels := append(baseLabels, "room")
 	roomTypedLabels := append(typedLabels, "room")
+	marketOrderLabels := append(roomTypedLabels, "order_type", "metric")
 
+	cpuShard = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: prefix + "cpu_shard",
+		Help: "CPU allocated to each shard",
+	}, baseLabels)
+	resources = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: prefix + "resources",
+		Help: "Resource amounts",
+	}, typedLabels)
+	marketOrders = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: prefix + "market_orders",
+		Help: "Market orders",
+	}, marketOrderLabels)
 	tick = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: prefix + "tick",
 		Help: "Current tick",
@@ -139,6 +167,9 @@ func setup() {
 		Buckets: []float64{.001, .005, .01, .05, .1, .25, .5, 1, 2.5, 5, 10, 30, 60},
 	})
 
+	prometheus.MustRegister(cpuShard)
+	prometheus.MustRegister(resources)
+	prometheus.MustRegister(marketOrders)
 	prometheus.MustRegister(tick)
 	prometheus.MustRegister(ms)
 	prometheus.MustRegister(cpu)
@@ -153,38 +184,42 @@ func setup() {
 	prometheus.MustRegister(processingDuration)
 }
 
-func getStatsFromMemory(shard string) (Stats, error) {
-	req, err := http.NewRequest("GET", "https://screeps.com/api/user/memory?shard="+shard, nil)
+func getStatsFromAuthMe() (AuthMeResponse, error) {
+	req, err := http.NewRequest("GET", "https://screeps.com/api/auth/me", nil)
 	if err != nil {
-		return Stats{}, err
+		return AuthMeResponse{}, err
 	}
 	req.Header.Set("X-Token", token)
 	resp, err := client.Do(req)
 	if err != nil {
-		return Stats{}, err
+		return AuthMeResponse{}, err
 	}
 	defer resp.Body.Close()
-	var memoryResponse MemoryResponse
-	err = json.NewDecoder(resp.Body).Decode(&memoryResponse)
+	var parsed AuthMeResponse
+	err = json.NewDecoder(resp.Body).Decode(&parsed)
 	if err != nil {
-		return Stats{}, err
+		return AuthMeResponse{}, err
 	}
-	data := strings.TrimPrefix(memoryResponse.Data, "gz:")
-	z, err := base64.StdEncoding.DecodeString(data)
+	return parsed, nil
+}
+
+func getMarketOrders() (MarketOrdersResponse, error) {
+	req, err := http.NewRequest("GET", "https://screeps.com/api/game/market/my-orders", nil)
 	if err != nil {
-		return Stats{}, err
+		return MarketOrdersResponse{}, err
 	}
-	r, err := gzip.NewReader(bytes.NewReader(z))
+	req.Header.Set("X-Token", token)
+	resp, err := client.Do(req)
 	if err != nil {
-		return Stats{}, err
+		return MarketOrdersResponse{}, err
 	}
-	defer r.Close()
-	var memory Memory
-	err = json.NewDecoder(r).Decode(&memory)
+	defer resp.Body.Close()
+	var parsed MarketOrdersResponse
+	err = json.NewDecoder(resp.Body).Decode(&parsed)
 	if err != nil {
-		return Stats{}, err
+		return MarketOrdersResponse{}, err
 	}
-	return memory.Stats.History[len(memory.Stats.History)-1], nil
+	return parsed, nil
 }
 
 func getStatsFromMemorySegment(shard string) (Stats, error) {
@@ -203,17 +238,27 @@ func getStatsFromMemorySegment(shard string) (Stats, error) {
 	if err != nil {
 		return Stats{}, err
 	}
-	var stats Stats
-	err = json.Unmarshal([]byte(memoryResponse.Data), &stats)
+	var parsed Stats
+	err = json.Unmarshal([]byte(memoryResponse.Data), &parsed)
 	if err != nil {
 		return Stats{}, err
 	}
-	return stats, nil
+	return parsed, nil
 }
 
 func updateStats() {
 	start := time.Now()
 
+	authMe, err := getStatsFromAuthMe()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	orders, err := getMarketOrders()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	var statsMap = make(map[string]Stats)
 	for _, shard := range shards {
 		stats, err := getStatsFromMemorySegment(shard)
@@ -224,6 +269,9 @@ func updateStats() {
 		statsMap[shard] = stats
 	}
 
+	cpuShard.Reset()
+	resources.Reset()
+	marketOrders.Reset()
 	rcl.Reset()
 	energy.Reset()
 	creeps.Reset()
@@ -231,41 +279,58 @@ func updateStats() {
 	storage.Reset()
 	terminal.Reset()
 
+	for shard, amount := range authMe.CPUShard {
+		cpuShard.With(prometheus.Labels{"shard": shard}).Set(amount)
+	}
+	resources.With(prometheus.Labels{"shard": "intershard", "type": "money"}).Set(authMe.Money)
+	for typ, amount := range authMe.Resources {
+		resources.With(prometheus.Labels{"shard": "intershard", "type": typ}).Set(amount)
+	}
+
+	for shard, orders := range orders.Shards {
+		for _, order := range orders {
+			marketOrders.With(prometheus.Labels{"shard": shard, "room": order.RoomName, "type": order.ResourceType, "order_type": order.Type, "metric": "price"}).Set(order.Price)
+			marketOrders.With(prometheus.Labels{"shard": shard, "room": order.RoomName, "type": order.ResourceType, "order_type": order.Type, "metric": "amount"}).Set(order.Amount)
+			marketOrders.With(prometheus.Labels{"shard": shard, "room": order.RoomName, "type": order.ResourceType, "order_type": order.Type, "metric": "remainingAmount"}).Set(order.RemainingAmount)
+			marketOrders.With(prometheus.Labels{"shard": shard, "room": order.RoomName, "type": order.ResourceType, "order_type": order.Type, "metric": "totalAmount"}).Set(order.TotalAmount)
+		}
+	}
+
 	for _, shard := range shards {
 		var stats = statsMap[shard]
-		tick.With(prometheus.Labels{"shard": shard}).Set(float64(stats.Tick))
-		ms.With(prometheus.Labels{"shard": shard}).Set(float64(stats.Ms))
+		tick.With(prometheus.Labels{"shard": shard}).Set(stats.Tick)
+		ms.With(prometheus.Labels{"shard": shard}).Set(stats.Ms)
 
-		cpu.With(prometheus.Labels{"shard": shard, "type": "used"}).Set(float64(stats.CPU.Used))
-		cpu.With(prometheus.Labels{"shard": shard, "type": "limit"}).Set(float64(stats.CPU.Limit))
-		cpu.With(prometheus.Labels{"shard": shard, "type": "bucket"}).Set(float64(stats.CPU.Bucket))
+		cpu.With(prometheus.Labels{"shard": shard, "type": "used"}).Set(stats.CPU.Used)
+		cpu.With(prometheus.Labels{"shard": shard, "type": "limit"}).Set(stats.CPU.Limit)
+		cpu.With(prometheus.Labels{"shard": shard, "type": "bucket"}).Set(stats.CPU.Bucket)
 
-		gcl.With(prometheus.Labels{"shard": shard, "type": "level"}).Set(float64(stats.GCL.Level))
-		gcl.With(prometheus.Labels{"shard": shard, "type": "progress"}).Set(float64(stats.GCL.Progress))
-		gcl.With(prometheus.Labels{"shard": shard, "type": "progressTotal"}).Set(float64(stats.GCL.ProgressTotal))
+		gcl.With(prometheus.Labels{"shard": shard, "type": "level"}).Set(stats.GCL.Level)
+		gcl.With(prometheus.Labels{"shard": shard, "type": "progress"}).Set(stats.GCL.Progress)
+		gcl.With(prometheus.Labels{"shard": shard, "type": "progressTotal"}).Set(stats.GCL.ProgressTotal)
 
-		gpl.With(prometheus.Labels{"shard": shard, "type": "level"}).Set(float64(stats.GPL.Level))
-		gpl.With(prometheus.Labels{"shard": shard, "type": "progress"}).Set(float64(stats.GPL.Progress))
-		gpl.With(prometheus.Labels{"shard": shard, "type": "progressTotal"}).Set(float64(stats.GPL.ProgressTotal))
+		gpl.With(prometheus.Labels{"shard": shard, "type": "level"}).Set(stats.GPL.Level)
+		gpl.With(prometheus.Labels{"shard": shard, "type": "progress"}).Set(stats.GPL.Progress)
+		gpl.With(prometheus.Labels{"shard": shard, "type": "progressTotal"}).Set(stats.GPL.ProgressTotal)
 
 		for name, room := range stats.Rooms {
-			rcl.With(prometheus.Labels{"shard": shard, "room": name, "type": "level"}).Set(float64(room.RCL.Level))
-			rcl.With(prometheus.Labels{"shard": shard, "room": name, "type": "progress"}).Set(float64(room.RCL.Progress))
-			rcl.With(prometheus.Labels{"shard": shard, "room": name, "type": "progressTotal"}).Set(float64(room.RCL.ProgressTotal))
+			rcl.With(prometheus.Labels{"shard": shard, "room": name, "type": "level"}).Set(room.RCL.Level)
+			rcl.With(prometheus.Labels{"shard": shard, "room": name, "type": "progress"}).Set(room.RCL.Progress)
+			rcl.With(prometheus.Labels{"shard": shard, "room": name, "type": "progressTotal"}).Set(room.RCL.ProgressTotal)
 
-			energy.With(prometheus.Labels{"shard": shard, "room": name, "type": "available"}).Set(float64(room.EnergyAvailable))
-			energy.With(prometheus.Labels{"shard": shard, "room": name, "type": "capacityAvailable"}).Set(float64(room.EnergyCapacityAvailable))
+			energy.With(prometheus.Labels{"shard": shard, "room": name, "type": "available"}).Set(room.EnergyAvailable)
+			energy.With(prometheus.Labels{"shard": shard, "room": name, "type": "capacityAvailable"}).Set(room.EnergyCapacityAvailable)
 
-			creeps.With(prometheus.Labels{"shard": shard, "room": name}).Set(float64(room.Creeps))
+			creeps.With(prometheus.Labels{"shard": shard, "room": name}).Set(room.Creeps)
 
 			for structureType, count := range room.Structures {
-				structures.With(prometheus.Labels{"shard": shard, "room": name, "type": structureType}).Set(float64(count))
+				structures.With(prometheus.Labels{"shard": shard, "room": name, "type": structureType}).Set(count)
 			}
 			for resourceType, amount := range room.Storage {
-				storage.With(prometheus.Labels{"shard": shard, "room": name, "type": resourceType}).Set(float64(amount))
+				storage.With(prometheus.Labels{"shard": shard, "room": name, "type": resourceType}).Set(amount)
 			}
 			for resourceType, amount := range room.Terminal {
-				terminal.With(prometheus.Labels{"shard": shard, "room": name, "type": resourceType}).Set(float64(amount))
+				terminal.With(prometheus.Labels{"shard": shard, "room": name, "type": resourceType}).Set(amount)
 			}
 		}
 	}
